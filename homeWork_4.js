e498f1cfb8f02f311330207ef718923ca8fa3a33
Abudagir <kondaktor3@gmail.com>
2023-07-31 23:13:33 +0200

    homeWork_14

--- homeWork_4.js
+++ homeWork_4.js
@@ -1,9 +1,9 @@
-const userText = prompt("Довільний рядок:");
-const textLength = prompt("Максимальна довжина рядка:");
-const maxLength = parseFloat(textLength);
+// const userText = prompt("Довільний рядок:");
+// const textLength = prompt("Максимальна довжина рядка:");
+// const maxLength = parseFloat(textLength);
 
-if (userText.length <= maxLength) {
-  console.log(userText);
-} else {
-  console.log(userText.substring(0, maxLength) + "...");
-}
+// if (userText.length <= maxLength) {
+//   console.log(userText);
+// } else {
+//   console.log(userText.substring(0, maxLength) + "...");
+// }
