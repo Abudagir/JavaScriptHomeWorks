a7e596114435b526d769262074ef635a097c2fb4
Abudagir <kondaktor3@gmail.com>
2023-08-12 00:23:08 +0200

    home_16

---
+++ homeWork_16.js
@@ -0,0 +1,22 @@
+const value = document.querySelector("#value");
+const container = document.querySelector(".button-container");
+let count = 0;
+
+const handleContainerClick = (e) => {
+  const eventName = e.target.dataset.action;
+  switch (eventName) {
+    case "decrease":
+      count -= 1;
+      break;
+    case "increase":
+      count += 1;
+      break;
+    case "reset":
+      count = 0;
+      break;
+    default:
+      return;
+  }
+  value.innerHTML = count;
+};
+container.addEventListener("click", handleContainerClick);
